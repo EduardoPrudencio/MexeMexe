0bf79cd108981b27af582fe62807dcf3a7b9d6d2
BIZVOX\eduardo.prudencio <eduardo.lobo@bemovin.com.br>
2019-04-02 13:12:08 -0300

    iniciando a exibicao de imagens de cartaexibicao da mao do jogador

--- MexeMexe/CodigoFonte/MexeMexe/MexeMexe.Aprexentacao.Wpf/ViewModel/GameViewModel.cs
+++ MexeMexe/CodigoFonte/MexeMexe/MexeMexe.Aprexentacao.Wpf/ViewModel/GameViewModel.cs
@@ -1,8 +1,38 @@
-﻿namespace MexeMexe.Aprexentacao.Wpf.ViewModel
+﻿using MexeMexe.Dominio.Modelo;
+using MexeMexe.Implementacao.Engine;
+using MexeMexe.Infraestrutura.Conteudo;
+
+namespace MexeMexe.Aprexentacao.Wpf.ViewModel
 {
     public class GameViewModel : ViewModelBase
     {
+        Configuracao _config;
+        Engine       _engine ;
+        Jogador player;
 
-        public string textoTeste { get { return "Funcionando"; } set { textoTeste = value;NotifyPropertyChange(nameof(textoTeste));  } }
+        string nomeDaCarta = string.Empty;
+
+        public GameViewModel()
+        {
+            _config = new Configuracao("Marquinhos", 2);
+            _engine = new Engine(_config);
+            player = _engine.ObterJogador();
+
+            nomeDaCarta = $"{player.Mao[0].Simbolo.ToString().ToLower()}{player.Mao[0].Nipe}";
+
+            
+        }
+
+        public string textoTeste {
+            get
+            {
+                return $"../img/{nomeDaCarta}.png";
+            }
+            set
+            {
+                textoTeste = value;NotifyPropertyChange(nameof(textoTeste));
+            }
+        }
+        
     }
 }
